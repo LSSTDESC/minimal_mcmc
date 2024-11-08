874342f2730ee6854260bb8b8fe19b9483e5c20e
aferte <aferte@roe.ac.uk>
2024-11-07 19:58:57 -0800

    Add example submision script.

---
+++ jobscript_perlmutter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --qos=regular
+#SBATCH --constraint=haswell
+#SBATCH -J desc-tutorial
+#SBATCH -n 100
+#SBATCH --cpus-per-task=1
+#SBATCH --constraint=cpu
+#SBATCH -t 03:00:00
+#SBATCH -o lssty1shear.log
+#SBATCH -e lssty1shear.error
+
+# Put here the path to the desc tutorial repo location
+cd $SCRATCH/desc/minimal_mcmc
+
+# Load environment
+source /global/cfs/cdirs/lsst/groups/MCP/setup_forecasts_int.sh
+
+# Run the chain
+time srun cosmosis --mpi forecast_3x2pt.ini
